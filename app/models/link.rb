ba55e88b11018fbee786df6272cbc241fbbd50be
Patrick McGreevy <patrick7490@icloud.com>
2014-04-22 17:53:21 -0700

    add comments to db

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
+  has_many :comments, as: :commentable
   validates :title, presence: true
   validates :content_url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422234900) do
+ActiveRecord::Schema.define(version: 20140423004624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "content"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "links", force: true do |t|
     t.string   "title"
